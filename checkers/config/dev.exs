0267fb30985b4eaa41e3e512c7b4c64c03712be0
baileylayton <bailey.layton.kay@gmail.com>
2018-03-16 16:08:45 -0400

    BK: removed default index page & updated dev db

--- checkers/config/dev.exs
+++ checkers/config/dev.exs
@@ -51,8 +51,8 @@ config :phoenix, :stacktrace_depth, 20
 # Configure your database
 config :checkers, Checkers.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
+  username: "checkers",
+  password: ";ZNh$72d]/A:",
   database: "checkers_dev",
   hostname: "localhost",
   pool_size: 10
