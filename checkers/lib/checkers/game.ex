679d3223a191fa137a9afa55ec3e663bae3c22a6
jsemeraro <julia.semeraro@outlook.com>
2018-03-27 19:39:32 -0400

    can skip over checkers and they disappear - bug inUI w/ red checkers

--- checkers/lib/checkers/game.ex
+++ checkers/lib/checkers/game.ex
@@ -4,7 +4,7 @@ defmodule Checkers.Game do
   def start_link(name) do
     b = new_game()
     Agent.start_link(fn ->
-      %{name: name, board: b, player1: nil, player2: nil} #, turn: :red}
+      %{name: name, board: b, player1: nil, player2: nil, turn: :red}
     end, name: reg(name))
   end
 
@@ -62,7 +62,8 @@ defmodule Checkers.Game do
         if new_col == 8 or new_col == -1 do
           []
         else
-          if Map.get(board, next_row) |> Map.get(new_col) |> Map.get(:color) == :none do
+          new_color = Map.get(board, next_row) |> Map.get(new_col) |> Map.get(:color)
+          if new_color == :none do
             [{next_row, new_col}]
           else
             []
@@ -88,22 +89,28 @@ defmodule Checkers.Game do
 
     if (new_row == -1 or new_row == 8) do
       possible_moves
+    else
+      IO.inspect(new_row)
+      IO.inspect (col_left)
+      IO.inspect(col_right)
+
+      left_moves =
+        case col_left do
+          -1 -> []
+          _ -> det_poss_moves(board, new_row, col_left, curr_color, -1) #++ possible_moves
+        end
+
+      right_moves =
+        case col_right do
+          8 -> []
+          _ -> det_poss_moves(board, new_row, col_right, curr_color, 1) #++ possible_moves
+        end
+
+      possible_moves = left_moves ++ right_moves
+      IO.inspect possible_moves
+      possible_moves
     end
 
-    left_moves =
-      case col_left do
-        -1 -> []
-        _ -> det_poss_moves(board, new_row, col_left, curr_color, -1) ++ possible_moves
-      end
-
-    right_moves =
-      case col_right do
-        8 -> []
-        _ -> det_poss_moves(board, new_row, col_right, curr_color, 1) ++ possible_moves
-      end
-    IO.inspect(left_moves)
-    IO.inspect(right_moves)
-    left_moves ++ right_moves
   end
 
   # TODO: write this function
@@ -113,9 +120,9 @@ defmodule Checkers.Game do
     our_tile? = Map.get(board, row_or) |> Map.get(col_or) |> Map.get(:color) == player
     next_empty_tile? = Map.get(board, row_dest) |> Map.get(col_dest) |> Map.get(:color) == :none
     legal_tile? = Map.get(board, row_dest) |> Map.get(col_dest) |> Map.get(:tile) == :playable
-    # our_turn? = Agent.get(agent, fn(state) -> Map.get(state, :turn) end) == player
+    our_turn? = Agent.get(agent, fn(state) -> Map.get(state, :turn) end) == player
 
-    if (our_tile? and next_empty_tile? and legal_tile? ) do
+    if (our_tile? and next_empty_tile? and legal_tile? and our_turn?) do
       possible_moves = get_poss_locations(board, origin)
       (length(possible_moves) > 0) and (dest in possible_moves)
     else
@@ -127,40 +134,76 @@ defmodule Checkers.Game do
   # to:   (row, col)
   def move_checker(agent, origin, dest, player) do
     if (valid_move(agent, origin, dest, player)) do
-      update_board(agent, origin, dest)
+      update_board(agent, origin, dest, player)
     end
   end
 
   # TODO: delete the checker we are possibly jumping over
   # update the board when we have a valid move
-  defp update_board(agent, origin = {row_or, col_or}, dest = {row_dest, col_dest}) do
+  defp update_board(agent, origin = {row_or, col_or}, dest = {row_dest, col_dest}, player) do
     board = Agent.get(agent, fn(state) -> Map.get(state, :board) end)
     origin_data = get_tile_data(board, origin)
     dest_data = get_tile_data(board, dest)
 
-    # row_diff = abs(row_or - row_dest)
-    # col_diff = abs(col_or - col_dest)
-    # if (row_diff > 1 and col_diff > 1) do
+    row_diff = row_or - row_dest
+    col_diff = col_or - col_dest
 
-    # end
 
-    row = Map.get(board, row_dest)
-    row = Map.put(row, col_dest, origin_data)
-    board = Map.put(board, row_dest, row)
+    if (abs(row_diff) == 2 and abs(col_diff) == 2) do
+      row_to_skip = trunc(row_or - (row_diff / 2))
+      col_to_skip = trunc(col_or - (col_diff / 2))
 
-    row = Map.get(board, row_or)
-    row = Map.put(row, col_or, dest_data)
-    board = Map.put(board, row_or, row)
+      row = Map.get(board, row_to_skip) |> Map.put(col_to_skip, dest_data)
+      board = Map.put(board, row_to_skip, row)
 
-    Agent.update(agent, fn(state) -> Map.put(state, :board, board) end)
+      row = Map.get(board, row_dest) |> Map.put(col_dest, origin_data)
+      board = Map.put(board, row_dest, row)
+
+      row = Map.get(board, row_or) |> Map.put(col_or, dest_data)
+      board = Map.put(board, row_or, row)
+
+      Agent.update(agent, fn(state) -> Map.put(state, :board, board) end)
+      Agent.update(agent, fn(state) ->
+        player1 = Map.get(state, :player1)
+        player2 = Map.get(state, :player2)
+        origin_color = Map.get(origin_data, :color)
+        if player1 |> Map.get(:color) == origin_color do
+          player1_score = player1 |> Map.get(:score)
+          Map.put(state, :player1, %{color: :red, score: player1_score+1})
+        else
+          player2_score = player2 |> Map.get(:score)
+          Map.put(state, :player2, %{color: :red, score: player2_score+1})
+        end
+      end)
+
+      IO.inspect dest
+      possible_moves = get_poss_locations(board, dest)
+      if Enum.empty?(possible_moves) do
+        if (Agent.get(agent, fn(state) -> Map.get(state, :turn) end) == :red) do
+          Agent.update(agent, fn(state) -> Map.put(state, :turn, :black) end)
+        else
+          Agent.update(agent, fn(state) -> Map.put(state, :turn, :red) end)
+        end
+      end
+
+    else
+      row = Map.get(board, row_dest) |> Map.put(col_dest, origin_data)
+      board = Map.put(board, row_dest, row)
+
+      row = Map.get(board, row_or) |> Map.put(col_or, dest_data)
+      board = Map.put(board, row_or, row)
+
+      Agent.update(agent, fn(state) -> Map.put(state, :board, board) end)
+
+      if (Agent.get(agent, fn(state) -> Map.get(state, :turn) end) == :red) do
+        Agent.update(agent, fn(state) -> Map.put(state, :turn, :black) end)
+      else
+        Agent.update(agent, fn(state) -> Map.put(state, :turn, :red) end)
+      end
+    end
     # next_moves = get_poss_locations(board, dest)
 
     # if Enum.empty?(next_moves) do
-    #   if (Agent.get(agent, fn(state) -> Map.get(state, :turn) end) == :red) do
-    #     Agent.update(agent, fn(state) -> Map.put(state, :turn, :black) end)
-    #   else
-    #     Agent.update(agent, fn(state) -> Map.put(state, :turn, :red) end)
-    #   end
     # end
   end
 
