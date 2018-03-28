8735ce5b677b7afef185fc9c537787fbcdd82e12
jsemeraro <julia.semeraro@outlook.com>
2018-03-27 22:26:19 -0400

    fixed prod

--- checkers/config/prod.exs
+++ checkers/config/prod.exs
@@ -58,7 +58,7 @@ config :logger, level: :info
 #
 #     config :checkers, CheckersWeb.Endpoint, server: true
 #
-
+config :logger, :console, format: "[$level] $message\n"
 # Finally import the config/prod.secret.exs
 # which should be versioned separately.
-import_config "prod.secret.exs"
+# import_config "prod.secret.exs"
